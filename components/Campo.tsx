654f4c46196131d2985ec390c6df5a0f88b7e82f
Dacu <daddek9@gmail.com>
2024-05-28 23:53:08 +0200

    logica data tempo

---
+++ components/Campo.tsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import { Text, TextInput, View, StyleSheet } from "react-native";
+import { primary_color, tertiary_color } from '../global';
+
+const Campo = (props: any) => (
+    <View style={style.row}>
+        <Text style={[style.text, {color: tertiary_color(props.tema)}]}>{props.nome.toUpperCase()}</Text>
+        <TextInput 
+            placeholder={"Inserisci " + props.nome}
+            placeholderTextColor="#888" 
+            keyboardType={props.tipo ? 'default' : props.tipo} 
+            style={[style.textinput, {backgroundColor: primary_color(props.tema)}]} 
+            value={props.value} 
+            onChangeText={props.onChange} />
+    </View>
+)
+
+
+const style = StyleSheet.create({
+    textinput: {
+        backgroundColor: 'white',
+        borderRadius: 15,
+        shadowColor: '#000',
+        shadowOffset: {
+            width: 0,
+            height: 2,
+        },
+        shadowOpacity: 0.25,
+        shadowRadius: 4,
+        elevation: 5,
+    },
+    row: {
+        marginBottom: 20, 
+        width: '80%' 
+    },
+    text: {
+        color: 'white',
+        fontWeight: 'bold',
+        marginBottom: 10
+    },
+
+})
+export default Campo
--- components/Input.tsx
+++ components/Input.tsx
@@ -4,10 +4,12 @@ import { View, StyleSheet, Text, TextInput, Button, TouchableOpacity, ScrollView
 import { ImageBackground } from 'react-native';
 import DatePicker, { getToday, getFormatedDate } from 'react-native-modern-datepicker'
 import TimePicker from '@react-native-community/datetimepicker'
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import Campo from './Campo';
 
 
 
-const Input = ({navigation} : any) => {
+const Input = ({ navigation }: any) => {
 
     const [openCalendar, setOpenCalendar] = useState(false)
     const [openClock, setOpenClock] = useState(false)
@@ -25,133 +27,124 @@ const Input = ({navigation} : any) => {
     const [data, setData] = useState(new Date())
     const [ora, setOra] = useState(new Date())
 
-    return(
-        
-        <ScrollView style = {style.ex}>
-            <View style={style.header}>
-                <Text style={style.listHeadline}>Nuovo Esame</Text>        
-            </View>
-        <ImageBackground style = {style.image} source = {require('../immaginiEsami/Onda2.png')}>
-        <View style = {style.container}> 
-            <View style = {style.row}>
-                <Text style = {style.text}>NOME</Text>
-                <TextInput placeholder= 'Inserisci Nome' placeholderTextColor="#888" style = {style.textinput} />
-            </View>
-            <View style = {style.row}>
-                <Text style = {style.text}>CORSO</Text>
-                <TextInput placeholder= 'Inserisci Corso' placeholderTextColor="#888" style = {style.textinput}/>            
-            </View>
-            <View style = {style.row}>
-                <Text style = {style.text}>VOTO</Text>
-                <TextInput placeholder= 'Inserisci Voto' placeholderTextColor="#888" keyboardType='numeric' style = {style.textinput}/>
-            </View>
-            <View style = {style.row}>
-                <Text style = {style.text}>CFU</Text>
-                <TextInput placeholder= 'Inserisci CFU' placeholderTextColor="#888" keyboardType='numeric' style = {style.textinput}/>
-            </View>
 
-            <View style = {style.row}>
-                <Text style = {style.text}>TIPOLOGIA</Text>
-                <TextInput placeholder= 'Inserisci Tipologia' placeholderTextColor="#888" style = {style.textinput}/>
-            </View>
+    const getTema = async () =>
+        (await AsyncStorage.getItem('tema') === 'dark')
 
-            <View style = {style.row}>
-                <Text style = {style.text}>DOCENTE</Text>
-                <TextInput placeholder= 'Inserisci Docente' placeholderTextColor="#888" style = {style.textinput}/>
-            </View> 
+    const [tema, setTema] = useState(true)
 
-            <View style = {style.row}>
-                <Text style = {style.text}>LUOGO</Text>
-                <TextInput placeholder= 'Inserisci Luogo' placeholderTextColor="#888" style = {style.textinput}/>
-            </View>  
+    useEffect(() => {
+        getTema().then(value => setTema(value))
+    }, [])
 
-            <View style={style.calendarContainer}>
-                    <TouchableOpacity onPress={()=>setOpenCalendar(true)}>
-                        <Text style={style.dataora}>INSERISCI DATA & ORA</Text>
-                    </TouchableOpacity>
-                </View>
-                
-                <Modal
-                    animationType='slide'
-                    transparent={true}
-                    visible={openCalendar}
-                    onRequestClose={() => setOpenCalendar(false)}
-                >
-                    <Pressable onPress={() => setOpenCalendar(false)} style={style.centeredView} android_disableSound={true} android_ripple={{ color: 'transparent' }}>
-                        <Pressable style={style.modalView} onPress={(e) => e.preventDefault()} android_disableSound={true} android_ripple={{ color: 'transparent' }}>
-                            <DatePicker
-                                mode='calendar'
-                                selected={data.toString()}
-                                onDateChange={()=>{setOpenCalendar(false);setOpenClock(true)}}
-                                options={{
-                                    textHeaderColor: secondary_color,
-                                    mainColor: secondary_color,
-                                    borderColor: secondary_color,
-                                    textSecondaryColor: secondary_color,
-                                }}
-                            />
-                        </Pressable>
-                    </Pressable>
-                </Modal>
-                {openClock ?
-                    <TimePicker
-                        mode='time'
-                        value={ora}
-                        minuteInterval={5}
-                        onChange={(_, selectedDate) => {(selectedDate?setOra(selectedDate):null);setOpenClock(false)}}
-                        onError={()=>setOpenClock(false)}
-                    /> : null
-                }        
 
-            <View style = {style.buttons}>
-                <TouchableOpacity style = {style.confirm}>
-                    <Text style = {style.confirmText}>CONFERMA</Text>
-                </TouchableOpacity>
-                <TouchableOpacity style = {style.deny}>
-                    <Text onPress={() => navigation.goBack()} style = {style.denyText}>ANNULLA</Text>
-                </TouchableOpacity>
-            </View>
-        </View>
-        </ImageBackground>
-        </ScrollView>
-    )
+    const temaStyle = StyleSheet.create({
+        color: {
+            color: tertiary_color(tema),
+        },
+        backgroundColor: {
+            backgroundColor: primary_color(tema),
+        },
+        appColor: {
+            color: secondary_color,
+        }
+    })
+
+    const formatData = (d: String) => {
+        const [yyyy, mm, dd] = d.split('/')
+        setData(new Date(parseInt(yyyy), parseInt(mm) - 1, parseInt(dd), 2 )) //Todo sostituire 2 con fuso orario locale
     }
 
+    const formatTime = (d: Date) => {
+        setOra(d) //todo Errore di fuso orario, non setta la data
+    }
+
+    console.log(data)
+
+    return (
+
+        <ScrollView style={style.ex}>
+            <View style={style.header}>
+                <Text style={style.listHeadline}>Nuovo Esame</Text>
+            </View>
+            <ImageBackground style={style.image} source={require('../immaginiEsami/Onda2.png')}>
+                <View style={style.container}>
+                    <Campo tema={tema} nome='Nome' value={nome} onChange={setNome} />
+                    <Campo tema={tema} nome='Corso' value={corso} onChange={setCorso} />
+                    <Campo tema={tema} nome='Voto' value={voto} onChange={setVoto} tipo='numeric' />
+                    <Campo tema={tema} nome='CFU' value={cfu} onChange={setCfu} tipo='numeric' />
+                    <Campo tema={tema} nome='Tipologia' value={tipologia} onChange={setTipologia} />
+                    <Campo tema={tema} nome='Docente' value={docente} onChange={setDocente} />
+                    <Campo tema={tema} nome='Luogo' value={luogo} onChange={setLuogo} />
+                    <View style={style.calendarContainer}>
+                        <TouchableOpacity onPress={() => setOpenCalendar(true)}>
+                            <Text style={style.dataora}>INSERISCI DATA & ORA</Text>
+                        </TouchableOpacity>
+                    </View>
+
+                    <Modal
+                        animationType='slide'
+                        transparent={true}
+                        visible={openCalendar}
+                        onRequestClose={() => setOpenCalendar(false)}
+                    >
+                        <Pressable onPress={() => setOpenCalendar(false)} style={style.centeredView} android_disableSound={true} android_ripple={{ color: 'transparent' }}>
+                            <Pressable style={style.modalView} onPress={(e) => e.preventDefault()} android_disableSound={true} android_ripple={{ color: 'transparent' }}>
+                                <DatePicker
+                                    mode='calendar'
+                                    selected={getFormatedDate(data, "YYYY/MM/DD")}
+                                    onDateChange={(val) => {formatData(val); setOpenCalendar(false); setOpenClock(true); }}
+                                    options={{
+                                        backgroundColor: primary_color(tema),
+                                        textHeaderColor: secondary_color,
+                                        mainColor: secondary_color,
+                                        borderColor: secondary_color,
+                                        textDefaultColor: tertiary_color(tema),
+                                        textSecondaryColor: secondary_color,
+                                    }}
+                                    
+                                />
+                            </Pressable>
+                        </Pressable>
+                    </Modal>
+                    {openClock ?
+                        <TimePicker
+                            mode='time'
+                            minuteInterval={5}
+                            value={data}
+                            onChange={(_, selectedDate) => { (selectedDate ? formatTime(selectedDate) : null); setOpenClock(false) }}
+                            onError={() => setOpenClock(false)}
+                        /> : null
+                    }
+
+                    <View style={style.buttons}>
+                        <TouchableOpacity style={style.confirm}>
+                            <Text style={style.confirmText}>CONFERMA</Text>
+                        </TouchableOpacity>
+                        <TouchableOpacity style={style.deny}>
+                            <Text onPress={() => navigation.goBack()} style={style.denyText}>ANNULLA</Text>
+                        </TouchableOpacity>
+                    </View>
+                </View>
+            </ImageBackground>
+        </ScrollView>
+    )
+}
+
 const style = StyleSheet.create({
-    
+
     image: {
         flex: 1,
     },
-    
+
     container: {
         flex: 1,
         alignItems: 'center',
         paddingTop: 10
     },
 
-    row: {
-        marginBottom: 20, 
-        width: '80%' 
-    },
 
-    text: {
-        color: 'white',
-        fontWeight: 'bold',
-        marginBottom: 10
-    },
 
-    textinput: {
-        backgroundColor: 'white',
-        borderRadius: 15,
-        shadowColor: '#000',
-        shadowOffset: {
-            width: 0,
-            height: 2,
-        },
-        shadowOpacity: 0.25,
-        shadowRadius: 4,
-        elevation: 5,
-    },
 
     buttons: {
         display: 'flex',
@@ -175,7 +168,7 @@ const style = StyleSheet.create({
 
     deny: {
         backgroundColor: '#e1dee3',
-        borderRadius: 5, 
+        borderRadius: 5,
         margin: 20,
         padding: 10,
         shadowColor: '#000',
@@ -196,8 +189,8 @@ const style = StyleSheet.create({
     denyText: {
         color: 'black',
         fontWeight: 'bold'
-    },  
-    
+    },
+
     ex: {
         flex: 1
     },
@@ -205,15 +198,15 @@ const style = StyleSheet.create({
     header: {
         flexDirection: 'row',
         alignItems: 'center',
-        height:60
+        height: 60
     },
 
     listHeadline: {
         color: '#333',
         fontSize: 22,
         fontWeight: 'bold',
-        flex:1,
-        textAlign:'center',
+        flex: 1,
+        textAlign: 'center',
     },
 
     centeredView: {
