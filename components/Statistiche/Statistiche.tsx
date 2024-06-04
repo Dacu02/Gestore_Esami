bf07fd3e58b1e8d93d94075ea6eac6b3c7af5adf
Dacu <daddek9@gmail.com>
2024-06-05 00:18:10 +0200

    Fix e refactor

--- components/Statistiche/Statistiche.tsx
+++ components/Statistiche/Statistiche.tsx
@@ -2,52 +2,42 @@ import AsyncStorage from '@react-native-async-storage/async-storage';
 import React, { useEffect, useState, useContext } from 'react';
 import { DataBaseContext } from "../DataBase"
 import SQLite from 'react-native-sqlite-storage'
-import { View, Animated, StyleSheet, Dimensions, ScrollView, SafeAreaView, FlatList, ActivityIndicator } from 'react-native';
-import { primary_color, secondary_color, tertiary_color } from '../../global';
-import { Avatar, Button, Card, Text, SegmentedButtons } from 'react-native-paper';
+import { View, StyleSheet, Dimensions, ScrollView, FlatList } from 'react-native';
+import { primary_color, rapportoOrizzontale, secondary_color, tertiary_color } from '../../global';
+import { Card, Text } from 'react-native-paper';
 import Header from '../Header';
 import {
   LineChart,
-  BarChart,
-  PieChart,
   ProgressChart,
-  ContributionGraph,
-  StackedBarChart
 } from "react-native-chart-kit";
-import { Item } from 'react-native-paper/lib/typescript/components/Drawer/Drawer';
-import { getFormatedDate } from 'react-native-modern-datepicker';
-import AnimatedProgressChart from './AnimatedProgressChart';
+import { MultiSelect } from 'react-native-element-dropdown';
 type EsameItem = {
   voto: string,
   data: Date,
   nome: string,
   cfu: number,
-}
-
-type ListaProps = {
-  esami: EsameItem[],
-  tema: boolean,
+  categoria: string[],
 }
 
 type Dato = {
-  nome: String
+  nome: string
   valore: number
 }
+
 
 const Statistiche = () => {
 
   const db = useContext(DataBaseContext) as SQLite.SQLiteDatabase
   const [esame, setEsame] = useState<EsameItem[]>([])
-  const [loading, setLoading] = useState(true);
+  const [vista, setVista] = useState<EsameItem[]>([])
   const [analitiche, setAnalitiche] = useState<Dato[] | null>([])
-
-  //dati del diagramma rotondo "progress-chart"
-  const data = {
-    labels: ["Esami passati", "CFU "], // optional
-    data: [0.4, 0.6]
-  };
+  const [dataCerchio, setDataCerchio] = useState({
+    labels: ["Esami", "CFU"],
+    data: [0, 0]
+  });
+  const [categoria, setCategoria] = useState<string[]>([])
+  const [categorieInserite, setCategorieInserite] = useState([] as string[])
   const loadData = () => {
-
     const dati: EsameItem[] = [];
     db.transaction((tx) => tx.executeSql('select nome, voto, data, cfu from esame where voto is not null order by data desc', [], (t, results) => {
       for (let i = 0; i < results.rows.length; i++) {
@@ -60,11 +50,36 @@ const Statistiche = () => {
           lode: results.rows.item(i).lode,
           cfu: results.rows.item(i).cfu,
           data: new Date(yyyy, mm, dd),
+          categoria: [] as string[],
         }
+        t.executeSql('select categoria from appartiene where esame = ?', [results.rows.item(i).nome], (_, res) => {
+          for (let j = 0; j < res.rows.length; j++)
+            esame.categoria.push(res.rows.item(j).categoria)
+        })
         dati.push(esame);
 
       }
 
+      const temp = [] as string[]
+
+      tx.executeSql('select nome from categoria', [], (_, res) => {
+        for (let i = 0; i < res.rows.length; i++)
+          temp.push(res.rows.item(i).nome)
+        setCategorieInserite(temp)
+      })
+
+      tx.executeSql('select cfu from esame where voto is null', [], (_, res) => {
+        const nonPassati = res.rows.length
+        const newDataCerchio = { ...dataCerchio }
+        newDataCerchio.data = [dati.length / (dati.length + nonPassati), 0] // rapporto esami passati su totali
+        let cfuOttenuti = 0
+        dati.map((item) => cfuOttenuti += item.cfu)
+        let cfuTotali = 0
+        for (let i = 0; i < res.rows.length; i++)
+          cfuTotali += res.rows.item(i).cfu
+        newDataCerchio.data[1] = cfuOttenuti / (cfuOttenuti + cfuTotali) // rapporto cfuOttenuti su cfu totali
+        setDataCerchio(newDataCerchio)
+      })
       let media = 0
       dati.map((item) => media += parseInt(item.voto))
       media = media / dati.length
@@ -74,7 +89,6 @@ const Statistiche = () => {
       let cfuTot = 0
       dati.map((item) => cfuTot += item.cfu)
       ponderata = ponderata / cfuTot
-
       const stats = dati.length !== 0 ? [
         {
           nome: 'Voto minimo',
@@ -91,33 +105,95 @@ const Statistiche = () => {
         {
           nome: 'Media ponderata',
           valore: ponderata
+        },
+        {
+          nome: 'Voto di laurea',
+          valore: ponderata * 110 / 30
         }
       ] : null
 
       setEsame(dati);
-      setLoading(false);
+      setVista(dati);
       setAnalitiche(stats)
     }));
 
   };
 
+
+  const getPlaceHolder = () => {
+    if (categoria.length === 0)
+      return 'Seleziona categoria'
+    let str = categoria[0]
+    for (let i = 1; i < categoria.length; i++)
+      str += ', ' + categoria[i]
+    return str
+  }
+
+
+
+  useEffect(() => {
+    let media = 0
+    vista.map((item) => media += parseInt(item.voto))
+    media = media / vista.length
+
+    let ponderata = 0
+    vista.map((item) => ponderata += parseInt(item.voto) * item.cfu)
+    let cfuTot = 0
+    vista.map((item) => cfuTot += item.cfu)
+    ponderata = ponderata / cfuTot
+
+    const stats = vista.length !== 0 ? [
+      {
+        nome: 'Voto minimo',
+        valore: Math.min(...vista.map((item) => parseInt(item.voto)))
+      },
+      {
+        nome: 'Voto massimo',
+        valore: Math.max(...vista.map((item) => parseInt(item.voto)))
+      },
+      {
+        nome: 'Media aritmetica',
+        valore: media
+      },
+      {
+        nome: 'Media ponderata',
+        valore: ponderata
+      }, {
+        nome: 'Voto di laurea',
+        valore:  analitiche ? analitiche[4].valore : 0
+      }
+    ] : null
+    setAnalitiche(stats)
+  }, [vista])
+
   const getTema = async () =>
     (await AsyncStorage.getItem('tema') === 'dark')
 
   const [tema, setTema] = useState(true)
+  const [width, setWidth] = useState(Dimensions.get('window').width)
 
   useEffect(() => {
     loadData()
     getTema().then(value => setTema(value))
-  }, [])
 
-  const [width, setWidth] = useState(Dimensions.get('window').width)
-  useEffect(() => {
     const updateLayout = () => {
       setWidth(Dimensions.get('window').width)
     }
     Dimensions.addEventListener('change', updateLayout)
   }, [])
+
+  useEffect(() => {
+    if (categoria.length === 0)
+      setVista(esame)
+    else {
+      const temp = [] as EsameItem[]
+      esame.forEach((item) => {
+        if (categoria.some((cat) => item.categoria.includes(cat)))
+          temp.push(item)
+      })
+      setVista(temp)
+    }
+  }, [categoria])
 
   return (
     <>
@@ -126,22 +202,41 @@ const Statistiche = () => {
         <Card style={[style.Card, { backgroundColor: primary_color(tema) }]}>
           <Card.Content style={style.Content}>
             <Text variant="titleLarge" style={{ color: tertiary_color(tema) }}>Analisi</Text>
-            <View style={[style.esito, { backgroundColor: "#99ffaf" }]}>
-              <Text style={[style.esitoText, { color: "#239900" }]}>Positivo</Text>
-            </View>
+            <MultiSelect
+              selectedTextStyle={{ color: tertiary_color(tema) }}
+              mode='auto'
+              search={false}
+              data={categorieInserite.map((v) => ({ value: v }))}
+              placeholder={getPlaceHolder()}
+              labelField={'value'}
+              valueField={'value'}
+              value={categoria}
+              key={'value'}
+              style={{ backgroundColor: primary_color(tema), width: '80%', paddingLeft: 20, borderRadius: 10, marginRight: '7.5%' }}
+              itemTextStyle={{ color: tertiary_color(tema) }}
+              placeholderStyle={{ color: tertiary_color(tema) + (categoria.length > 0 ? '' : '80') }}
+              onChange={setCategoria}
+              containerStyle={{ backgroundColor: primary_color(tema), borderRadius: 10, borderWidth: 0 }}
+              itemContainerStyle={{ backgroundColor: primary_color(tema), borderColor: secondary_color }}
+              selectedStyle={{ display: 'none' }}
+              activeColor={secondary_color + 'bb'}
+              renderItem={(item) => (
+                <Text style={[style.selectItem, { color: tertiary_color(tema) }]}>{item.value}</Text>
+              )}
+            />
           </Card.Content>
 
-          <Text style={[style.cardLabel, { color: tertiary_color(tema) }]} variant="bodyMedium">Grafico andamento esami</Text>
+          <Text style={[style.cardLabel, { color: tertiary_color(tema) }]}>Grafico andamento esami</Text>
 
           {
-            esame.length != 0 ?
+            vista.length != 0 ?
               <LineChart
 
                 data={{
-                  labels: esame.map((item) => item.nome),
+                  labels: vista.map((item) => item.nome),
                   datasets: [
                     {
-                      data: esame.map((item) => parseInt(item.voto)),
+                      data: vista.map((item) => parseInt(item.voto)),
 
                     },
                   ]
@@ -176,12 +271,12 @@ const Statistiche = () => {
                 }}
 
               />
-              
+
               : null}
 
           <View style={[style.boxCentrale, { backgroundColor: primary_color(tema), height: 'auto' }]}>
             <View>
-              <Text style={[style.titolo, {color: tertiary_color(tema)}]}>Dati statistici</Text>
+              <Text style={[style.titolo, { color: tertiary_color(tema) }]}>Dati statistici</Text>
             </View>
             <FlatList
               horizontal
@@ -193,12 +288,31 @@ const Statistiche = () => {
 
                 </View>
               )}
-              keyExtractor={(item) => item.valore.toString()}
+              keyExtractor={(item) => item.nome}
 
             />
           </View>
-          <AnimatedProgressChart
-/>
+          <Card style={[style.Card, { backgroundColor: primary_color(tema) }]}>
+            <ProgressChart
+              data={dataCerchio}
+              width={width}
+              height={220}
+              strokeWidth={16}
+              radius={32}
+              chartConfig={{
+                backgroundGradientFrom: "#3d85c6",
+                backgroundGradientTo: "#073763",
+                color: (opacity = 1) => `rgba(26, 255, 146, ${opacity})`,
+                strokeWidth: 1,
+                barPercentage: 0.5,
+                useShadowColorFromDataset: false
+              }}
+              style={{
+                borderRadius: 16,
+              }}
+              hasLegend
+            />
+          </Card>
         </Card>
       </ScrollView>
     </>
@@ -222,9 +336,9 @@ const style = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'space-between'
   },
-  boxCentrale:{
-    marginBottom:20,
-    marginTop:10
+  boxCentrale: {
+    marginBottom: 20,
+    marginTop: 10
   },
   header: {
     marginTop: 10,
@@ -271,6 +385,17 @@ const style = StyleSheet.create({
     textAlign: 'center',
     justifyContent: 'center',
     fontSize: 22
+  },
+  selectList: {
+    borderRadius: 8,
+    borderColor: 'black',
+    borderWidth: 1,
+    padding: 10,
+    margin: 10
+  },
+  selectItem: {
+    padding: rapportoOrizzontale(10),
+    margin: rapportoOrizzontale(5),
   },
 });
 
